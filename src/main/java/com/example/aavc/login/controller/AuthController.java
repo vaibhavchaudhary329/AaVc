a10a0c4380a5e18d5585c64de08d140cec3967ea
Vaibhav Chaudhary <vaibhavchaudhary329@gmail.com>
2025-05-11 09:27:33 +0530

    login v1.0.0

---
+++ src/main/java/com/example/aavc/login/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.example.aavc.login.controller;
+
+import com.example.aavc.login.dto.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController{
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        String username = loginRequest.getUsername();
+        String password = loginRequest.getPassword();
+        try{
+            Authentication authentication=authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequest.getUsername(), loginRequest.getPassword()
+                    )
+            );
+            return ResponseEntity.ok("Login Successful");
+        }catch (BadCredentialsException badCredentialsException){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("InvalidCredential");
+        }
+    }
+}
