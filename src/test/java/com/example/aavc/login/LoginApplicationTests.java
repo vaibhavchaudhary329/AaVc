cbe8a51083668c5b6d5a8077886c48dad577a18f
Vaibhav Chaudhary <vaibhavchaudhary329@gmail.com>
2025-05-14 12:10:12 +0530

    Production Ready Beta Final

--- src/test/java/com/example/aavc/login/LoginApplicationTests.java
+++ src/test/java/com/example/aavc/login/LoginApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.aavc.login;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LoginApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
