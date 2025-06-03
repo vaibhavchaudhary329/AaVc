807ff98d86fe3a62eda5210e8f7cfcf09466ba74
Vaibhav Chaudhary <vaibhavchaudhary329@gmail.com>
2025-06-03 19:23:29 +0530

    Corsfilter(URL) updated in SecurityConfig

--- src/main/java/com/example/aavc/login/config/SecurityConfig.java
+++ src/main/java/com/example/aavc/login/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "https://your-frontend.netlify.app"
+                "https://aavc.onrender.com/"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
