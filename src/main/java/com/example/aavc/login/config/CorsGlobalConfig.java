e0dafd580349de235032b735dd2f39415acded1b
Vaibhav Chaudhary <vaibhavchaudhary329@gmail.com>
2025-06-05 22:41:04 +0530

    Render config update 4 "created CorsGlobalConfigr"

---
+++ src/main/java/com/example/aavc/login/config/CorsGlobalConfig.java
@@ -0,0 +1,17 @@
+package com.example.aavc.login.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsGlobalConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://aavc.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
