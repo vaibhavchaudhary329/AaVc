0e3b63e481c9b2c5f01acff4d31c7973ff5b3a09
Vaibhav Chaudhary <vaibhavchaudhary329@gmail.com>
2025-06-05 22:16:50 +0530

    Render config update 3 "Updated crossorigin on controller"

--- src/main/java/com/example/aavc/login/controller/ForgotPasswordController.java
+++ src/main/java/com/example/aavc/login/controller/ForgotPasswordController.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
-@CrossOrigin(origins =  "http://localhost:3000")
+@CrossOrigin(origins =  "https://aavc.netlify.app")
 @RestController
 @RequestMapping("/auth")
 public class ForgotPasswordController {
--- src/main/java/com/example/aavc/login/controller/UserController.java
+++ src/main/java/com/example/aavc/login/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://aavc.netlify.app")
 
 @RestController
 @RequestMapping("/user")
