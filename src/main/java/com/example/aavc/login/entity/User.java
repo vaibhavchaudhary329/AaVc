9b5ed31244e455c847ee7487302d5cb6e0a9c5ff
Vaibhav Chaudhary <vaibhavchaudhary329@gmail.com>
2025-06-13 00:56:38 +0530

    Implemented: GetProfile ProfileUpdate

--- src/main/java/com/example/aavc/login/entity/User.java
+++ src/main/java/com/example/aavc/login/entity/User.java
@@ -11,12 +11,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(unique = true)
+    private String mobile;
     @Column(unique = true)
     private String email;
     @Column(unique = true)
     private String username;
     private String password;
+    private String fullName;
     private String role;
     @Column(name = "reset_token")
     private String resetToken;
@@ -24,16 +26,20 @@ public class User {
     private LocalDateTime tokenExpiry;
 
     //Getter
+    public String getMobile(){ return mobile;}
     public String getEmail(){ return email;}
     public String getUsername(){ return username;}
     public String getPassword(){return password;}
+    public String getFullName(){return fullName;}
     public String getRole(){ return role;}
     public String getResetToken(){ return resetToken;}
     public LocalDateTime getTokenExpiry(){ return tokenExpiry;}
 
     //Setter
+    public void setMobile(String mobile){this.mobile=mobile;}
     public void setEmail(String email){this.email=email;}
     public void setUsername(String username){this.username=username;}
+    public void setFullName(String fullName){this.fullName=fullName;}
     public void setPassword(String password){this.password=password;}
     public void setRole(String role){this.role=role;}
     public void setResetToken(String resetToken) { this.resetToken=resetToken;}
