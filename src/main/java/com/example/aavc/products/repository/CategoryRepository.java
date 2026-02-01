4d4384ef606c04b967150c8af3c4d0d0c8f18238
Vaibhav Chaudhary <vaibhavchaudhary329@gmail.com>
2026-02-01 22:25:40 +0530

    ProductsAPI hotfix

--- src/main/java/com/example/aavc/products/repository/CategoryRepository.java
+++ src/main/java/com/example/aavc/products/repository/CategoryRepository.java
@@ -2,9 +2,11 @@ package com.example.aavc.products.repository;
 
 import com.example.aavc.products.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByNameIgnoreCase(String name);
 }
